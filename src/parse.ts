301b196eb559a65155a68596c911eea1f65408cd
Pooya Parsa <pyapar@gmail.com>
2020-12-16 00:10:19 +0100

    remove unnecessary generic

--- src/parse.ts
+++ src/parse.ts
@@ -21,14 +21,14 @@ export function hasProtocol (inputStr: string): boolean {
   return /^\w+:\/\//.test(inputStr)
 }
 
-export function entriesToObject<T = string> (entries: IterableIterator<[string, T]>): Record<string, T | T[]> {
-  const obj: Record<string, T | T[]> = {}
+export function entriesToObject (entries: IterableIterator<[string, string]>): Record<string, string|string[]> {
+  const obj: Record<string, string | string[]> = {}
   for (const [key, value] of entries) {
     if (obj[key]) {
       if (Array.isArray(obj[key])) {
-        (obj[key] as T[]).push(value)
+        (obj[key] as string[]).push(value)
       } else {
-        obj[key] = [(obj[key] as T), value]
+        obj[key] = [obj[key] as string, value]
       }
     } else {
       obj[key] = value
