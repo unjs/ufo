a43a7f4917815db9ad214227554bc1b8e3394eb6
Harlan Wilton <harlan@harlanzw.com>
2022-11-24 15:17:24 +1100

    chore: linting

--- test/filter.test.ts
+++ test/filter.test.ts
@@ -1,80 +1,80 @@
-import { describe, expect, test } from 'vitest'
-import { createFilter } from '../src'
+import { describe, expect, test } from "vitest";
+import { createFilter } from "../src";
 
-describe('createFilter', () => {
+describe("createFilter", () => {
   const excludeFilter = createFilter({
-    exclude: ['/admin/**', '/admin', /^\/regex\/.*/]
-  })
+    exclude: ["/admin/**", "/admin", /^\/regex\/.*/]
+  });
 
   const valid: string[] = [
-    '/foo',
-    '/foo/',
-    '/foo/bar',
-    '/foo/bar/',
-    '/bar/foo/',
-    '/prefixed/admin/',
-    '/some/path/regex/may/catch'
-  ]
+    "/foo",
+    "/foo/",
+    "/foo/bar",
+    "/foo/bar/",
+    "/bar/foo/",
+    "/prefixed/admin/",
+    "/some/path/regex/may/catch"
+  ];
   const invalid: string[] = [
-    '/admin',
-    '/admin/',
-    '/admin/test',
-    '/admin/foo/some-really-long/url',
-    '/regex/path',
-    '/regex/'
-  ]
+    "/admin",
+    "/admin/",
+    "/admin/test",
+    "/admin/foo/some-really-long/url",
+    "/regex/path",
+    "/regex/"
+  ];
 
   for (const t of valid) {
     test(`excludeFilter: ${t} valid`, () => {
-      expect(excludeFilter(t)).toBeTruthy()
-    })
+      expect(excludeFilter(t)).toBeTruthy();
+    });
   }
   for (const t of invalid) {
     test(`excludeFilter: ${t} invalid`, () => {
-      expect(excludeFilter(t)).toBeFalsy()
-    })
+      expect(excludeFilter(t)).toBeFalsy();
+    });
   }
 
   const includeFilter = createFilter({
-    include: ['/admin/**', '/admin', /^\/regex\/.*/]
-  })
+    include: ["/admin/**", "/admin", /^\/regex\/.*/]
+  });
 
   for (const t of valid) {
     test(`includeFilter: ${t} invalid`, () => {
-      expect(includeFilter(t)).toBeFalsy()
-    })
+      expect(includeFilter(t)).toBeFalsy();
+    });
   }
   for (const t of invalid) {
     test(`includeFilter: ${t} valid`, () => {
-      expect(includeFilter(t)).toBeTruthy()
-    })
+      expect(includeFilter(t)).toBeTruthy();
+    });
   }
 
   const bothFilter = createFilter({
-    include: ['/admin/**', /^\/regex\/.*/],
-    exclude: ['/admin/foo/**', /^\/regex\/foo\/.*/]
-  })
+    include: ["/admin/**", /^\/regex\/.*/],
+    exclude: ["/admin/foo/**", /^\/regex\/foo\/.*/]
+  });
 
-  for (const t of ['/admin/fo', '/regex/bar']) {
+  for (const t of ["/admin/fo", "/regex/bar"]) {
     test(`bothFilter: ${t} valid`, () => {
-      expect(bothFilter(t)).toBeTruthy()
-    })
+      expect(bothFilter(t)).toBeTruthy();
+    });
   }
-  for (const t of ['/admin/foo/', '/regex/foo/test']) {
+  for (const t of ["/admin/foo/", "/regex/foo/test"]) {
     test(`bothFilter: ${t} invalid`, () => {
-      expect(bothFilter(t)).toBeFalsy()
-    })
+      expect(bothFilter(t)).toBeFalsy();
+    });
   }
-})
+});
 
-describe('createFilter example', () => {
+describe("createFilter example", () => {
   const adminUrl = createFilter({
-    include: ['/admin/**', '/admin', /^\/_api\/\d*/]
-  })
+    include: ["/admin/**", "/admin", /^\/_api\/\d*/]
+  });
 
-  for (const t of ['/admin', '/admin/', '/admin/foo', '/admin/foo/bar', '/_api/123']) {
+  for (const t of ["/admin", "/admin/", "/admin/foo", "/admin/foo/bar", "/_api/123"]) {
     test(`adminUrl: ${t} valid`, () => {
-      expect(adminUrl(t)).toBeTruthy()
-    })
+      expect(adminUrl(t)).toBeTruthy();
+    });
   }
-})
+});
