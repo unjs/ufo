d07d5a8b5d84dcc06aa0658a7dae538caec073ca
Daniel Roe <daniel@roe.dev>
2024-02-05 13:40:28 +0000

    style: lint

--- src/parse.ts
+++ src/parse.ts
@@ -128,7 +128,10 @@ export function stringifyParsedURL(parsed: Partial<ParsedURL>): string {
   const hash = parsed.hash || "";
   const auth = parsed.auth ? parsed.auth + "@" : "";
   const host = parsed.host || "";
-  const proto = parsed.protocol || parsed[protocolRelative] ? (parsed.protocol || "") + "//" : "";
+  const proto =
+    parsed.protocol || parsed[protocolRelative]
+      ? (parsed.protocol || "") + "//"
+      : "";
   return proto + auth + host + pathname + search + hash;
 }
 
